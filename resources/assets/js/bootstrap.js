16c404405b9d64f7786ea4217fb7bafcf8546925
Pavel Koch <pavel.koch@wrongware.cz>
2016-10-22 11:33:46 +0100

    travis, stylci

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,6 +1,3 @@
-
-window._ = require('lodash');
-
 /**
  * We'll load jQuery and the Bootstrap jQuery plugin which provides support
  * for JavaScript based Bootstrap features such as modals and tabs. This
@@ -20,26 +17,23 @@ window.Vue = require('vue');
 require('vue-resource');
 
 /**
+ * Access tokens
+ */
+
+window.Sobapi = {
+  csrfToken: document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
+  apiToken: document.querySelector('meta[name="api-token"]').getAttribute('content'),
+};
+
+/**
  * We'll register a HTTP interceptor to attach the "CSRF" header to each of
  * the outgoing requests issued by this application. The CSRF middleware
  * included with Laravel will automatically verify the header's value.
  */
 
 Vue.http.interceptors.push((request, next) => {
-    request.headers.set('X-CSRF-TOKEN', Laravel.csrfToken);
+    request.headers.set('X-CSRF-TOKEN', Sobapi.csrfToken);
+    request.headers.set('Authorization', `Bearer ${Sobapi.apiToken}`);
 
     next();
 });
-
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
-
-// import Echo from "laravel-echo"
-
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: 'your-pusher-key'
-// });
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,11 +8,13 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    <passport-clients></passport-clients>
-                    <passport-authorized-clients></passport-authorized-clients>
-                    <passport-personal-access-tokens></passport-personal-access-tokens>
+                    Welcome!
                 </div>
             </div>
+
+            <passport-clients></passport-clients>
+            <passport-authorized-clients></passport-authorized-clients>
+            <passport-personal-access-tokens></passport-personal-access-tokens>
         </div>
     </div>
 </div>

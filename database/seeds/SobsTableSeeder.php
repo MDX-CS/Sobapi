174c38aa6bc1e14d35dc1f725888322badef1623
Pavel Koch <pavel.koch@wrongware.cz>
2016-11-12 21:36:54 +0000

    Table seeder fix

--- database/seeds/SobsTableSeeder.php
+++ database/seeds/SobsTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class SobsTableSeeder extends Seeder
@@ -12,7 +13,10 @@ class SobsTableSeeder extends Seeder
     public function run()
     {
         factory(App\Models\Sob::class, 5)->create()->each(function (App\Models\Sob $sob) {
-            $sob->students()->attach(env('LDAP_USERNAME', 'user'));
+            $sob->students()->attach(env('LDAP_USERNAME', 'user'), [
+                'observed_by' => env('LDAP_USERNAME', 'user'),
+                'observed_on' => Carbon::now(),
+            ]);
             $sob->level()->associate(rand(1, 3));
             $sob->topic()->associate(rand(1, 4));
             $sob->save();
